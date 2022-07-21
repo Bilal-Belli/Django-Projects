4af4effe518d32c37e93e3d3eaab3f47846c31f8
Bilal <jb_belli@esi.dz>
2022-07-21 15:56:30 +0100

    ajouter des donnes par le fichier views

--- src/src/blog/views.py
+++ src/src/blog/views.py
@@ -1,5 +1,27 @@
+from multiprocessing import context
 from django.shortcuts import render
 
+posts = [ # c'est des exemples des enregistrements
+    {
+        'title': 'first blog',
+        'content': 'le contenu test',
+        'author': 'bilalbelli'
+    },
+    {
+        'title': 'second blog',
+        'content': 'le contenu test',
+        'author': 'bilalbelli'
+    },
+    {
+        'title': 'third blog',
+        'content': 'le contenu test',
+        'author': 'bilalbelli'
+    }
+]
 # Create your views here.
 def home(request):
-    return render(request ,'blog\index.html', {'title':'home'})
+    context = { #c'est l'enregistrement de la page
+        'title': 'home page',
+        'posts': posts
+    }
+    return render(request ,'blog\index.html', context)
