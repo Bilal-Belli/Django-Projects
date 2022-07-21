4cf31f5ae24a9b61491788aba532515598e0a152
Bilal <jb_belli@esi.dz>
2022-07-21 16:53:03 +0100

    ajoute les donnes et la configuration admin par le api django

--- src/src/blog/admin.py
+++ src/src/blog/admin.py
@@ -1,3 +1,5 @@
+from pdb import post_mortem
 from django.contrib import admin
-
+from .models import post
 # Register your models here.
+admin.site.register(post)
--- src/src/blog/models.py
+++ src/src/blog/models.py
@@ -1,3 +1,16 @@
+from tkinter import CASCADE
+from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+#python manage.py makemigrations    to create sql model of this class in migrations folder
+#python manage.py sqlmigrate blog 0001
+#python manage.py migrate 
